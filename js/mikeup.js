9b7af7219027aa1eff3b9a5f81911ac51d6d491b
Mike Upchurch <mupchrch@users.noreply.github.com>
2015-11-08 20:14:24 -0500

    Implement about tab

--- js/mikeup.js
+++ js/mikeup.js
@@ -1,9 +1,43 @@
-$(window).load(loadCss);
+$(window).load(initializePage);
 
-function loadCss() {
+function initializePage() {
+  loadHeader();
+  loadHome();
+
+  $(".home").click(loadHome);
+  $(".about").click(loadAbout);
+  $(".contact").click(loadContact);
+
+  if (document.location.hash === "#home") {
+    loadHome();
+  } else if (document.location.hash === "#about") {
+    loadAbout();
+  } else if (document.location.hash === "#contact") {
+
+  }
+}
+
+function loadHeader() {
   $(".header").addClass("loadHeader");
   $(".belowHeader").addClass("loadOpacity");
   $(".menuItems").addClass("loadOpacity");
   $(".backgroundImage").addClass("loadOpacity");
+}
+
+function loadHome() {
   $(".bigText").addClass("loadBigText");
+
+  $(".backgroundImage").removeClass("moveImageRight");
+  $(".aboutPanel").removeClass("loadAboutPanel");
+  $(".backgroundImageCover").removeClass("loadHalfOpacity");
+}
+
+function loadAbout() {
+  $(".backgroundImage").addClass("moveImageRight");
+  $(".aboutPanel").addClass("loadAboutPanel");
+  $(".backgroundImageCover").addClass("loadHalfOpacity");
+}
+
+function loadContact() {
+
 }
