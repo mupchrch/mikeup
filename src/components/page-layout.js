ceeed85eef4bcbc55578180aa1922e234168affc
Mike Upchurch <mupchrch@users.noreply.github.com>
2019-10-08 21:07:47 -0400

    adds page transition wrapper to animate panels

--- src/components/page-layout.js
+++ src/components/page-layout.js
@@ -1,13 +1,32 @@
 import React from "react";
 import Header from "./header";
 import Hero from "./hero";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
-const PageLayout = (props) => {
+class Transition extends React.PureComponent {
+  render() {
+    const { children, location } = this.props;
+
+    return (
+      <TransitionGroup component={null}>
+        <CSSTransition
+          key={location.pathname}
+          timeout={500}
+          classNames="page"
+        >
+          {children}
+        </CSSTransition>
+      </TransitionGroup>
+    );
+  }
+}
+
+const PageLayout = ({ children, location }) => {
   return (
     <>
       <Header />
       <Hero />
-      {props.children}
+      <Transition location={location}>{children}</Transition>
     </>
   );
 }
