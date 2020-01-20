543efaf02809c9b94e400c32becc90ff14e22b9b
Mike Upchurch <mupchrch@users.noreply.github.com>
2020-01-20 17:11:47 -0500

    Adds animated stars to background

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './style.module.scss';
 
 const Hero = () => {
+  const [isAnimating, setAnimating] = useState(true);
+  const animationClassName = isAnimating ? null : styles.paused;
+
   return (
     <div className={styles.hero}>
+      <button onClick={()=> setAnimating(!isAnimating)}>
+        {isAnimating ? 'pause' : 'play'}
+      </button>
+      <div className={`${styles.farStars} ${animationClassName}`} />
+      <div className={`${styles.midStars} ${animationClassName}`} />
+      <div className={`${styles.nearStars} ${animationClassName}`} />
       <div className={styles.bigText}>
         Software Developer
       </div>
