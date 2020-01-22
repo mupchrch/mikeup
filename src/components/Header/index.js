ce4a292677af445c0dac2d4ceced0eb0b0d53dde
Mike Upchurch <mupchrch@users.noreply.github.com>
2020-01-21 19:06:14 -0500

    adds ripple to header items and fixes window resize underline

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,29 @@
-import React, { forwardRef, useCallback, useState, useEffect } from 'react';
+import React, { forwardRef, useCallback, useState, useEffect, useRef } from 'react';
 import { Link } from 'gatsby';
 import styles from './style.module.scss';
 
+const menuItems = [{ text: 'Home', link: '/', emoji: '🏡' }, { text: 'About', link: '/about', emoji: '📰' }, { text: 'Contact', link: '/contact', emoji: '👋' }];
+
 const Header = ({ currentPath }) => {
+  const selectedItemRef = useRef(null); // used for window resize
   const [selectedDecoratorStyles, setSelectedDecoratorStyles] = useState({ transform: 'translateX(0)', width: 0 });
 
-  const menuItems = [{ text: 'Home', link: '/', emoji: '🏡' }, { text: 'About', link: '/about', emoji: '📰' }, { text: 'Contact', link: '/contact', emoji: '👋' }];
-
-  useEffect(() => {
-    const handleWindowResize = () => {
-      // TODO handle resize of underline
-    };
-    window.addEventListener('resize', handleWindowResize);
-    return () => window.removeEventListener('resize', handleWindowResize);
-  });
-
-  // useCallback so it is known when the ref changes elements
-  const selectedItemRef = useCallback(element => {
+  // need to know when the ref changes elements
+  const selectedItemCallbackRef = useCallback(element => {
+    selectedItemRef.current = element; // keep track of a ref for use in window resize
+    
     if (element !== null) {
       setSelectedDecoratorStyles({ transform: `translateX(${element.offsetLeft}px)`, width: element.offsetWidth });
     }
+  }, []);
+
+  // handle resize of underline
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setSelectedDecoratorStyles({ transform: `translateX(${selectedItemRef.current.offsetLeft}px)`, width: selectedItemRef.current.offsetWidth });
+    };
+    window.addEventListener('resize', handleWindowResize);
+    return () => window.removeEventListener('resize', handleWindowResize);
   }, []);
 
   return (
@@ -29,7 +33,7 @@ const Header = ({ currentPath }) => {
       </div>
       <ul className={styles.menu}>
         {menuItems.map(({ text, link, emoji }) => (
-          <MenuItem key={link} text={text} link={link} emoji={emoji} ref={link === currentPath ? selectedItemRef : null} />
+          <MenuItem key={link} text={text} link={link} emoji={emoji} ref={link === currentPath ? selectedItemCallbackRef : null} />
         ))}
         <div className={styles.selectedDecorator} style={selectedDecoratorStyles} />
       </ul>
