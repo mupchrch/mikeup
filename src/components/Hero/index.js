0f3f04e6784ab7d99f6eb6ad5c664b37d1389865
Mike Upchurch <mupchrch@users.noreply.github.com>
2020-01-24 14:19:39 -0500

    adds local storage hook for background anim

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -5,7 +5,7 @@ import { CSSTransition } from 'react-transition-group';
 import styles from './style.module.scss';
 
 const Hero = ({ isHome }) => {
-  const [isAnimating, setAnimating] = useState(true);
+  const [isAnimating, setAnimating] = useLocalStorage('isBackgroundAnimating', true);
   const animationClassName = isAnimating ? '' : styles.paused;
 
   return (
@@ -37,6 +37,43 @@ const Hero = ({ isHome }) => {
       </div>
     </CSSTransition>
   );
+}
+
+// https://usehooks.com/useLocalStorage/
+function useLocalStorage(key, initialValue) {
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      // Get from local storage by key
+      const item = window.localStorage.getItem(key);
+      // Parse stored json or if none return initialValue
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // If error also return initialValue
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = value => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
 }
 
 export default Hero;
