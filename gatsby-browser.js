56ecc232e7119c7efc5847c4d1f7db08a56a2476
Mike Upchurch <mupchrch@users.noreply.github.com>
2020-01-31 09:18:12 -0500

    fixes code highlight in posts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,9 @@
-// custom typefaces
-import 'typeface-montserrat';
-import 'typeface-merriweather';
-import './src/styles/global.scss';
 
 import React from 'react';
 import PageLayout from './src/components/PageLayout';
+
+import './src/styles/global.scss';
+import 'prismjs/themes/prism-tomorrow.css';
 
 export const wrapPageElement = ({ element, props }) => (
   <PageLayout {...props}>{element}</PageLayout>
